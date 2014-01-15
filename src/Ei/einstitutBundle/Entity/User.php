6f8a5fe85341b5fd5a845ecf00b88c4adaa5a5a3
TARIK-S <TARIK-S@10.0.2.15>
2014-01-15 16:07:51 +0100

    Doctrine Modification de la Classe User

--- src/Ei/einstitutBundle/Entity/User.php
+++ src/Ei/einstitutBundle/Entity/User.php
@@ -14,15 +14,6 @@ class User extends BaseUser
 {
 
 
-
-    /**
-    * @ORM\OneToMany(targetEntity="Evenements", mappedBy="evenements_user")
-    */
-    private $evenement;
-
-
-
-
     /**
     * @ORM\OneToMany(targetEntity="Tutoriels_EnLigne", mappedBy="user_tuto")
     */
@@ -64,6 +55,198 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+     /**
+     * @var string
+     *
+     * @ORM\Column(name="Avatar", type="string", length=255)
+     */
+    private $avatar;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Sexe", type="string", length=255)
+     */
+    private $sexe;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Tel", type="string", length=255)
+     */
+    private $tel;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Pays", type="string", length=255)
+     */
+    private $pays;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Ville", type="string", length=255)
+     */
+    private $ville;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Adresse", type="string", length=300)
+     */
+    private $adresse;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set avatar
+     *
+     * @param string $avatar
+     * @return Ut
+     */
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+    
+        return $this;
+    }
+
+    /**
+     * Get avatar
+     *
+     * @return string 
+     */
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    /**
+     * Set sexe
+     *
+     * @param string $sexe
+     * @return Ut
+     */
+    public function setSexe($sexe)
+    {
+        $this->sexe = $sexe;
+    
+        return $this;
+    }
+
+    /**
+     * Get sexe
+     *
+     * @return string 
+     */
+    public function getSexe()
+    {
+        return $this->sexe;
+    }
+
+    /**
+     * Set tel
+     *
+     * @param string $tel
+     * @return Ut
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+    
+        return $this;
+    }
+
+    /**
+     * Get tel
+     *
+     * @return string 
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * Set pays
+     *
+     * @param string $pays
+     * @return Ut
+     */
+    public function setPays($pays)
+    {
+        $this->pays = $pays;
+    
+        return $this;
+    }
+
+    /**
+     * Get pays
+     *
+     * @return string 
+     */
+    public function getPays()
+    {
+        return $this->pays;
+    }
+
+    /**
+     * Set ville
+     *
+     * @param string $ville
+     * @return Ut
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+    
+        return $this;
+    }
+
+    /**
+     * Get ville
+     *
+     * @return string 
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     * @return Ut
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string 
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
 
     public function __construct()
     {
@@ -71,7 +254,6 @@ class User extends BaseUser
         parent::__construct();
         // your own logic
     }
-    
-    
+
     
 }
