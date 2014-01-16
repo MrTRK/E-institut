d4b60e0187e94f46c712908f565eeb8572ee6a5a
TARIK-S <TARIK-S@10.0.2.15>
2014-01-16 21:41:29 +0100

    Définition des actions dans le controleur

--- src/Ei/einstitutBundle/Controller/DefaultController.php
+++ src/Ei/einstitutBundle/Controller/DefaultController.php
@@ -6,25 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('EieinstitutBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('EieinstitutBundle:Pages:index.html.twig');
     }
-     public function adminAction()
+    public function ressourcesAction()
     {
-        return $this->render('EieinstitutBundle:Default:admin.html.twig');
+        return $this->render('EieinstitutBundle:Ressources:listes_ressources.html.twig');
     }
-    public function pageAction()
+    
+    public function forumAction()
     {
-        return $this->render('EieinstitutBundle::Sub-Master-Right.html.twig');
+        return $this->render('EieinstitutBundle:Community:Forum.html.twig');
     }
-
-	public function espace_personnelAction()
-    {
-        return $this->render('EieinstitutBundle::espace_personnel.html.twig');
-    }
-    public function test_pageAction()
-    {
-        return $this->render('EieinstitutBundle:Pages:Licence.html.twig');
-    }
+    
 }
