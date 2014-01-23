044533dae624bb64a37ac9b2e68a1a0232ab44cf
zsz <zsz@192.168.1.13>
2014-01-23 17:22:32 +0100

    bundle inegration admin

--- src/Ei/AdministrationBundle/Controller/DefaultController.php
+++ src/Ei/AdministrationBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('EiAdministrationBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('EiAdministrationBundle:Default:Master-Base-Admin.html.twig');
     }
 }
