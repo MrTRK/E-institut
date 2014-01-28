ddb6de1a7fddd2e7c187d68782e8d50d8ce87bbb
zhang <zhang@10.0.2.15>
2014-01-28 16:58:36 +0100

    gestion administration

--- src/Ei/AdministrationBundle/Controller/DefaultController.php
+++ src/Ei/AdministrationBundle/Controller/DefaultController.php
@@ -8,6 +8,40 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('EiAdministrationBundle:Default:Master-Base-Admin.html.twig');
+        return $this->render('EiAdministrationBundle:admin:tbd_admin.html.twig');
+    }
+    public function actualiteAction()
+    {
+        return $this->render('EiAdministrationBundle:admin:a_liste_actualite.html.twig');
+    }
+    public function pageAction()
+    {
+        return $this->render('EiAdministrationBundle:admin:a_liste_page.html.twig');
+    }
+    
+        
+     public function ressourcesAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $rs= $em->getRepository('EieinstitutBundle:Fiche')->findAll();
+        return $this->render('EiAdministrationBundle:admin:a_liste_ressource.html.twig',
+                            array(
+                                'ressource' => $rs
+                            )    
+                );
+        
+    }
+    
+    public function listeuserAction()
+    {
+        return $this->render('EiAdministrationBundle:admin:a_liste_user.html.twig');
+    }
+    public function listetutoAction()
+    {
+        return $this->render('EiAdministrationBundle:admin:a_liste_tuto.html.twig');
+    }
+     public function newsletterAction()
+    {
+        return $this->render('EiAdministrationBundle:admin:a_liste_newsletter.html.twig');
     }
 }
